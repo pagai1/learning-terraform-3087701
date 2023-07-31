29e51f1294aa7abab2dce9412cb6cf45339709ba
pagai1 <35419595+pagai1@users.noreply.github.com>
2023-07-31 13:37:26 +0200

    create dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 
@@ -27,6 +40,8 @@ resource "aws_instance" "blog" {
 
   tags = {
     Name = "Learning Terraform"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -65,4 +80,17 @@ resource "aws_security_group_rule" "blog_http_everything_out" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
+}
+
+module "blog_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  name = "blog"
+  vpc_id = module.vpc.public_subnets[0]
+ 
+  ingres_rules = ["http-80-tcp","https-443-tcp"]
+  ingres_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
