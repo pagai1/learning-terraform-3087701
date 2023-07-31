6123013f18410909dafefb73f133d086c54f5223
pagai1 <35419595+pagai1@users.noreply.github.com>
2023-07-31 13:39:14 +0200

    typo

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ resource "aws_instance" "blog" {
   }
 }
 
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in, allow everything out."
@@ -88,8 +89,8 @@ module "blog_sg" {
   name = "blog"
   vpc_id = module.vpc.public_subnets[0]
  
-  ingres_rules = ["http-80-tcp","https-443-tcp"]
-  ingres_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
